41b13f3524d3b4b9928fbd6a37858cd500bd7851
King George <georgeofosumensah@gmail.com>
2021-03-01 15:47:53 +0000

    I created a Q1.java class file to answer question 1

---
+++ src/answers/Q1.java
@@ -0,0 +1,30 @@
+package answers;
+
+class Electronics{
+	void device_type() {
+		
+	}
+}
+
+class Television extends Electronics{
+	
+	void category() {
+		System.out.println("Television");
+	}
+	
+}
+
+class LED extends Television{
+	void display_tech() {
+		System.out.println("The Technology is LED");
+	}
+}
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
